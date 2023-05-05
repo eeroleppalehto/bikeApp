1281119a26e41d4cfa9a2c0fb9ac685cbb9a599d
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-05 14:26:03 +0300

    Import cors and bikeRental router

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import stationRouter from './routes/stations';
+import bikeRentalRouter from './routes/bikeRentals';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3001;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api/stations', stationRouter);
+app.use('/api/bikeRentals', bikeRentalRouter);
 
 app.get('/ping', (_req, res) => {
   console.log('Ping pong!');
