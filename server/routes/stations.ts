f0f696e9449ced47b1165f0eb547f55be4501bf6
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-05 14:29:39 +0300

    Added post method to station router

--- server/routes/stations.ts
+++ server/routes/stations.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import stationService from '../services/stationService';
+import { ZodError } from 'zod';
 
 const stationRouter = express.Router();
 
@@ -10,10 +11,23 @@ stationRouter.get('/', async (_req, res) => {
   res.json(stations);
 });
 
-/* stationRouter.post('/', async (req, res) => {
-  const data = req.body;
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
+stationRouter.post('/', async (req, res) => {
+  try {
+    const newStation = await stationService.addNew(req.body);
+    res.json(newStation);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong';
 
+    if ( error instanceof ZodError) {
+      //console.log("error");
+      errorMessage = error.message;
+    }
+    
+    console.log(errorMessage);
+    res.status(400).send(errorMessage);
+  }
+});
 
-}); */
 
 export default stationRouter;
