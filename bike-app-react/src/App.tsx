ffc78cb965d60647f3fcb145cdc4ad5eb5b5fad2
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-05 14:22:51 +0300

    Test fetching data to frontend

--- bike-app-react/src/App.tsx
+++ bike-app-react/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import stationsService from './services/stationsService';
+import { Station } from './types';
 
 function App() {
+  const [stations, setStations] = useState<Station[]>([]);
+
+  useEffect(() => {
+    const fetchStations = async () => {
+      const stationList = await stationsService.getAll();
+
+      setStations(stationList);
+    };
+
+    fetchStations();
+  },[])
+
+  const StationComps = stations.map(station => <li key={station.id}>{station.name}</li>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello Woooorld</p>
+      <ul>
+        {StationComps}
+      </ul>
     </div>
   );
 }
