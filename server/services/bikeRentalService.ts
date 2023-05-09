247823043be65b19fa993f19eecef3d8af7ba923
eeroleppalehto <eero.leppalehto@gmail.com>
2023-05-09 10:13:13 +0300

    Checks if object is too large and add data in batches

--- server/services/bikeRentalService.ts
+++ server/services/bikeRentalService.ts
@@ -26,8 +26,13 @@ const addNew = async (object: unknown) => {
 };
 
 const addMany = async (object: unknown | unknown[]) => {
+  console.log('zodparsing...');
   const data = BikeRentalList.parse(object);
+  const MAX_INPUT_SIZE = 10000;
 
+  console.log('Complete!');
+
+  console.log('generate list for createMany...');
   const bikeRentalList: Prisma.BikeRentalUncheckedCreateInput[] = data.map(item => {
     const bikeRental = {
       departureTime: item.departureTime,
@@ -41,11 +46,44 @@ const addMany = async (object: unknown | unknown[]) => {
     return bikeRental;
   });
 
+
+  console.log('Complete!');
+
+  console.log('Add many to db...');
+
+  const totalCount: { count: number } = {
+    count: 0
+  };
+
+  if (bikeRentalList.length > MAX_INPUT_SIZE) {
+    console.log('Array size too large, splitting into smaller batches...');
+    let currentLength = bikeRentalList.length;
+    let i = 0;
+    while (currentLength>0) {
+      console.log(`Batch ${i+1}...`);
+      const count = await prisma.bikeRental.createMany({
+        data:bikeRentalList.slice(i*MAX_INPUT_SIZE,(i+1)*MAX_INPUT_SIZE),
+        skipDuplicates: true,
+      });
+      currentLength -= MAX_INPUT_SIZE;
+      totalCount.count += count.count;
+      i += 1;
+      console.log(`Batch ${i+1} done!`);
+    }
+  } else {
   const count = await prisma.bikeRental.createMany({
     data:bikeRentalList,
     skipDuplicates: true,
-  });
+    });
+    totalCount.count += count.count;
+  }
 
+
+
+  console.log('Transfer complete!');
+
+  return totalCount;
+};
   return count;
 };
 
